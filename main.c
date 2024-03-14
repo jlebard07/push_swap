9abc88289dc1288befd00150b5f80424b6577097
Jean Lebard <jlebard@bess-f4r2s4.clusters.42paris.fr>
2024-03-14 13:55:36 +0100

    struct

---
+++ main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlebard <jlebard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 13:35:15 by jlebard           #+#    #+#             */
+/*   Updated: 2024/03/14 13:41:19 by jlebard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	if (ft_error_check(argc) != NULL)
+	{
+		write (1, "Error", 5);
+		return (NULL);
+	}
+	// creer une fonction error
+	// creer un file plus une fonction pour chacune des fonctions
+}
---
+++ push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlebard <jlebard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 13:20:53 by jlebard           #+#    #+#             */
+/*   Updated: 2024/03/14 13:30:24 by jlebard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "ft_printf/ft_printf.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+#endif
