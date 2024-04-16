83f3cb114893ec6016fe6e6782a4b641e8d33489
Jean Lebard <jlebard@bess-f4r5s5.clusters.42paris.fr>
2024-04-16 14:06:32 +0200

    C

--- push.c
+++ push.c
@@ -6,22 +6,22 @@
 /*   By: jlebard <jlebard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:20:35 by jlebard           #+#    #+#             */
-/*   Updated: 2024/04/12 11:20:27 by jlebard          ###   ########.fr       */
+/*   Updated: 2024/04/16 11:28:08 by jlebard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-static void	push(t_stack_node **src_pile, t_stack_node dest_pile)
+static void	push(t_stack_node **src_pile, t_stack_node **dest_pile)
 {
 	t_stack_node	*temp;
 
-	if(*src_pile == NULL)
+	if (*src_pile == NULL)
 		return ;
 	temp = (*src_pile)->next;
-	(*src_pile)->next = *dest;
-	*dest = *src;
-	*src = temp;	
+	(*src_pile)->next = *dest_pile;
+	*dest_pile = *src_pile;
+	*src_pile = temp;
 }
 
 void	push_a(t_stack_node **a, t_stack_node **b)
--- reverse_rotate.c
+++ reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: jlebard <jlebard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:23:29 by jlebard           #+#    #+#             */
-/*   Updated: 2024/04/10 13:24:27 by jlebard          ###   ########.fr       */
+/*   Updated: 2024/04/16 11:32:08 by jlebard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 static void	reverse_rotate(t_stack_node **stack)
 {
-	t_stack_node	temp;
-	t_stack_node	bottom;
-	t_stack_node	temp2;
+	t_stack_node	*temp;
+	t_stack_node	*bottom;
+	t_stack_node	*temp2;
 
 	temp = *stack;
 	temp2 = *stack;
@@ -29,21 +29,21 @@ static void	reverse_rotate(t_stack_node **stack)
 	temp->next = NULL;
 }
 
-void	rra(t_stack_node a)
+void	rra(t_stack_node **a)
 {
-	reverse_rotate(*a);
+	reverse_rotate(a);
 	ft_putstr_fd("rra", 1);
 }
 
-void	rrb(t_stack_node b)
+void	rrb(t_stack_node **b)
 {
-	reverse_rotate(*b);
+	reverse_rotate(b);
 	ft_putstr_fd("rrb", 1);
 }
 
-void	rrr(t_stack_node a, t_stack_node b)
+void	rrr(t_stack_node **a, t_stack_node **b)
 {
-	reverse_rotate(*a);
-	reverse_rotate(*b);
+	reverse_rotate(a);
+	reverse_rotate(b);
 	ft_putstr_fd("rra", 1);
 }
